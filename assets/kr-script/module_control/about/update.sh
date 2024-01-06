0564120eb0381c573ae41525f8e867e67423a6a2
Explore <51757959+ZhangJinhui-baoji@users.noreply.github.com>
2024-01-06 13:18:50 +0800

    Update update.sh

--- assets/kr-script/module_control/about/update.sh
+++ assets/kr-script/module_control/about/update.sh
@@ -7,7 +7,7 @@ if [ -z "$latest_release" ]; then
 else
     current_version="2024.1.5"
     if [ "$latest_release" != "$current_version" ]; then
-        echo "发现新的发行版: $latest_release"
+        echo "发现新版本: $latest_release\n密码：zpk9"
     else
         echo "当前已是最新版本: $current_version"
     fi
