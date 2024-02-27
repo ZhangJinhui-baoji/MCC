2857f8fa838aafe2de6a4c7cd4b40db880e595ed
Explore <51757959+ZhangJinhui-baoji@users.noreply.github.com>
2024-02-27 16:35:42 +0800

    Add files via upload

--- assets/kr-script/about/update_log.sh
+++ assets/kr-script/about/update_log.sh
@@ -1,6 +1,6 @@
 repository="ZhangJinhui-baoji/MCC"
 
-latest_release=$(curl --connect-timeout 1 -s "https://api.github.com/repos/$repository/releases/latest" | awk -F'"' '/"tag_name": ".+"/{print $4}')
+latest_release=$(curl --connect-timeout 2 --max-time 1 -s "https://api.github.com/repos/$repository/releases/latest" | awk -F'"' '/"tag_name": ".+"/{print $4}')
 
 if [ -z "$latest_release" ]; then
     echo "无法获取发行版信息"
@@ -10,7 +10,7 @@ else
     if [ "$latest_release" != "$current_version" ]; then
         echo "*发现新版本: $latest_release\n"
 
-        changelog=$(curl --connect-timeout 1 -s "https://api.github.com/repos/$repository/releases/latest" | awk -v RS='"body":' -F '"' 'NR>1 {print $2}')
+        changelog=$(curl --connect-timeout 2 --max-time 1 -s "https://api.github.com/repos/$repository/releases/latest" | awk -v RS='"body":' -F '"' 'NR>1 {print $2}')
 
         changelog=$(echo -e "$changelog")
 
